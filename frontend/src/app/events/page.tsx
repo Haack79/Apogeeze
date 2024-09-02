64e934247b897db91c240d5cc2831aa94de0c87f
Brian Haack <haack79@hotmail.com>
2024-09-02 10:12:10 -0700

    updated styling for entry button

--- frontend/src/app/events/page.tsx
+++ frontend/src/app/events/page.tsx
@@ -1,26 +1,37 @@
-import Link from "next/link"
-import { Event, columns } from "./columns"
-import { DataTable } from "./data-table"
+import Link from "next/link";
+import { Event, columns } from "./columns";
+import { DataTable } from "./data-table";
 
 async function getData(): Promise<Event[]> {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}`)
-  if (!res.ok) {
-    throw new Error("failed to get all events")
+  const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+  
+  if (!backendUrl) {
+    throw new Error("Backend URL is not defined");
   }
-  const data: Event[] = await res.json()
+  const res = await fetch(backendUrl);
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch events");
+  }
+
+  const data: Event[] = await res.json();
   return data;
 }
 
 export default async function Page() {
-  const data = await getData()
-  // console.log(JSON.stringify(data))
+  const data = await getData();
+
   return (
     <div className="h-3/4 container mx-auto py-10 flex-col" suppressHydrationWarning>
       <div className="flex justify-center items-center mb-4">
-        <Link className="p-3 bg-blue-600 rounded-xl hover:bg-blue-400 text-white" href="/new-event">Create Event</Link>
+        <Link className="p-3 bg-blue-600 rounded-xl hover:bg-blue-400 text-white" href="/new-event">
+          Create Event
+        </Link>
       </div>
-      <p className="text-blue-600 text-semibold" suppressHydrationWarning>Click on a row to edit and a column header to sort.</p>
+      <p className="text-blue-600 text-semibold" suppressHydrationWarning>
+        Click on a row to edit and a column header to sort.
+      </p>
       <DataTable columns={columns} initialData={data} />
     </div>
-  )
+  );
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,12 @@
 import Link from "next/link";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center space-y-5 p-24" suppressHydrationWarning>
+    <span className="flex min-h-screen flex-col items-center space-y-5 p-24 bg-black" suppressHydrationWarning>
       <div className="font-semibold text-blue-600 text-2xl">Hello Apogeeze</div>
-      <Link className="p-3 bg-blue-600 rounded-xl hover:bg-blue-400 text-white" href="/events">Go to Chart</Link>
-    </main>
+      <div className="relative">
+        <div className="absolute -inset-3 bg-gradient-to-r from-yellow-500 to-orange-700 rounded-xl blur"></div>
+        <Link className="relative px-5 py-2 bg-blue-600 rounded-xl hover:bg-blue-400 text-white leading-none" href="/events">Go to Chart</Link>
+      </div>
+    </span>
   );
 }
